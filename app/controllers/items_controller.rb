de45e8e1ea636c347c67a3400f228130316c9567
maotaro0 <ma.prs.10@gmail.com>
2026-09-08 21:15:38 +0900

    修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,8 +48,7 @@ class ItemsController < ApplicationController
   end
 
   def check_owner
-    redirect_to root_path unless current_user == @item.user
-    redirect_to root_path if @item.order.present?
+    redirect_to root_path unless current_user == @item.user && @item.order.blank?
   end
 
   def item_params
